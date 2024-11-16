5209de6f2cd36f2f4d3cab4f5412525597f611ba
Viktoriya <viktoriya_trof@mail.ru>
2024-11-16 18:53:42 +0300

    brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import playGameEven from "../src/cod-brain-even.js";
+import playGameEven from '../src/cod-brain-even.js';
 import startGames from '../src/index.js';
 
 startGames(playGameEven);
